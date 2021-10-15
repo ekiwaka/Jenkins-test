51d5c1ef141b0af76f879759c3731069e32fc4a9
Karan <simplykaran13@gmail.com>
2021-10-15 15:39:56 +0200

    Add files via upload
    Simple product

---
+++ simple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int product(int x, int y) {
+   // printf() displays the product of two numbers
+   printf(x*y);
+   return 0;
+}
+
+
+int main(){
+	product (3,4);
+	return 0;
+}
