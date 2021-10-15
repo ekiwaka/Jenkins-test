abd63a7b64dd8befbca9a814bfb8bfc0fe714d1b
Karan <simplykaran13@gmail.com>
2021-10-15 15:45:48 +0200

    Add files via upload

---
+++ simple2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int product(int x, int y) {
+   // printf() displays the product of two numbers
+   printf("%d", x*y);
+   return 0;
+}
+
+
+int main(){
+	int var;
+	printf("Enter a number");
+	scanf("%d", &var)
+	product (3,var);
+	return 0;
+}
